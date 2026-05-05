3e73dc49535f1c0bdd1f765ce9c2353eea9c8db3
Nevo Biton <155353129+NevoBiton20@users.noreply.github.com>
2026-05-05 15:57:32 +0300

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -1,9 +1,41 @@
 def sorted_subset_sums(numbers: set):
-    # Put your iterator/generator here. 
-    pass
+    """
+    >>> list(sorted_subset_sums({1, 2, 4}))
+    [0, 1, 2, 3, 4, 5, 6, 7]
+
+    >>> list(sorted_subset_sums({1, 2, 3}))
+    [0, 1, 2, 3, 3, 4, 5, 6]
+
+    >>> list(sorted_subset_sums({2, 3, 4}))
+    [0, 2, 3, 4, 5, 6, 7, 9]
+
+    >>> list(sorted_subset_sums(set()))
+    [0]
+
+    >>> list(sorted_subset_sums({5}))
+    [0, 5]
+    """
+    nums = sorted(numbers)
+
+    yield 0
+
+    if not nums:
+        return
+
+    heap = [(nums[0], 0)]
+
+    while heap:
+        current_sum, i = heappop(heap)
+        yield current_sum
+
+        next_i = i + 1
+
+        if next_i < len(nums):
+            heappush(heap, (current_sum + nums[next_i], next_i))
+            heappush(heap, (current_sum - nums[i] + nums[next_i], next_i))
 
 if __name__ == '__main__':
-    # from itertools import takewhile, islice
-    # for i in eval(input()):
-    #     print(i, end=", ")
+     from itertools import takewhile, islice
+     for i in eval(input()):
+         print(i, end=", ")
     pass
